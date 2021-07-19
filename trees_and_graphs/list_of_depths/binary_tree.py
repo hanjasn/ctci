db8ec22731d9575507c24f5b948bde4fc4481e0c
hanjasn <hanjasn@gmail.com>
2021-07-19 15:56:51 -0700

    Edit 'List of Depths' solution

--- trees_and_graphs/list_of_depths/binary_tree.py
+++ trees_and_graphs/list_of_depths/binary_tree.py
@@ -19,20 +19,18 @@ class BinaryTreeNode:
     else:
       if self.right == None:
         self.right = BinaryTreeNode(data)
-      else: 
+      else:
         self.right.insert_in_order(data)
-  
+
   def print_in_order(self) -> str:
     result = "[" + self._print_in_order(self)
     result = result[0:-2] + "]"
     return result
 
   def _print_in_order(self, node: 'BinaryTreeNode') -> str:
-    if node == None:
-      return ""
-
     result = ""
-    result += self._print_in_order(node.left)
-    result += str(node.data) + ", "
-    result += self._print_in_order(node.right)
+    if node != None:
+      result += self._print_in_order(node.left)
+      result += str(node.data) + ", "
+      result += self._print_in_order(node.right)
     return result
